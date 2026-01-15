581556ce73906749729faa4eead5938c44c0176e
Quetzal <rmsmech@outlook.com>
2026-01-16 01:14:11 +0400

    modifications incoming for policies

--- HaleyAbstractionsCore/WorkFlowEngine/Enums/LifeCycleNoticeKind.cs
+++ HaleyAbstractionsCore/WorkFlowEngine/Enums/LifeCycleNoticeKind.cs
@@ -6,6 +6,7 @@ namespace Haley.Enums {
     public enum LifeCycleNoticeKind { 
         Error = 1, 
         Warning = 2, 
-        Info = 3 
+        Info = 3 ,
+        OverDue = 4 //When a specific instance remains in same state for too long but this is not tracked by time-out policies.. then we start sending overdue notice..
     }
 }
