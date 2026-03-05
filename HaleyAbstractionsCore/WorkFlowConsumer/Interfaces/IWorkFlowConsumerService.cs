be790efdca8742c7a598cd1d5a6bfa1e049bc65e
Quetzal <rmsmech@outlook.com>
2026-03-06 03:32:22 +0400

    Modifications to WorkFlow Consumer

--- HaleyAbstractionsCore/WorkFlowConsumer/Interfaces/IWorkFlowConsumerService.cs
+++ HaleyAbstractionsCore/WorkFlowConsumer/Interfaces/IWorkFlowConsumerService.cs
@@ -2,7 +2,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace Haley.Abstractions {
-    public interface IConsumerService {
+    public interface IWorkFlowConsumerService {
         Task StartAsync(CancellationToken ct = default);
         Task StopAsync(CancellationToken ct = default);
     }
